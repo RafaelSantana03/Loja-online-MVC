1c74aaa31d4e96d3788984d3321d3c02b456ea57
Rafael Santana <rafa03santana@gmail.com>
2025-07-08 21:48:32 -0300

    adicionando efeitos aos cards

--- obj/Debug/net8.0/LojaOnline.AssemblyInfo.cs
+++ obj/Debug/net8.0/LojaOnline.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LojaOnline")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88a2f6dffbf4ad271107c3d0680c66c60b37e52a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b860824102d54720e277212bdd7a5090434108d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LojaOnline")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LojaOnline")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
